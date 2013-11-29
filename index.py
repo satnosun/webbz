8c5bcdc00afd817c263cee48d0dc2233052763aa
Ningning Wang <satnosun@gmail.com>
2013-11-29 08:28:59 +0800

    copy from duapp

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print "hello"
+from flask import Flask, g, request, render_template
+ 
+app = Flask(__name__)
+app.debug = True
+ 
+@app.route('/')
+def hello():
+    return render_template('hello.html')
+ 
+from bae.core.wsgi import WSGIApplication
+application = WSGIApplication(app)
