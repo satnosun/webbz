7bb5783031f146c37be0c1c1619d4768ed48df04
satnosun <satnosun@gmail.com>
2013-12-04 00:04:19 +0800

    bs-js

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, request, render_template
+from flask import Flask, g, request, render_template, session, url_for, escape, redirect
  
 app = Flask(__name__)
 app.debug = True
@@ -7,6 +7,36 @@ app.debug = True
 @app.route('/index')
 def hello():
     return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
